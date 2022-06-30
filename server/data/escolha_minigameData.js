0d351717c1b87ce663132cb8782cb80fbe1fc084
joaomarcuscardoso <jm.cardoso@users.noreply.github.com>
2022-06-30 15:36:19 -0300

    get hours in getEscolhas(escolha_minigameData)

--- server/data/escolha_minigameData.js
+++ server/data/escolha_minigameData.js
@@ -13,7 +13,7 @@ exports.saveEscolhaMinigame = function (data) {
 
 exports.getEscolhas = function () {
   return database.manyOrNone(
-    "SELECT tem.identificador_original, tem.esta_certo, tem.texto, tem.botao_clicado, (SELECT apl.id_aluno FROM associacao_player_aluno apl WHERE apl.id_player = apem.id_player ) FROM associacao_player_escolha_minigame apem INNER JOIN tb_escolha_minigame tem ON apem.id_escolha_minigame = tem.id_escolha_minigame;"
+    "SELECT tem.horario, tem.identificador_original, tem.esta_certo, tem.texto, tem.botao_clicado, (SELECT apl.id_aluno FROM associacao_player_aluno apl WHERE apl.id_player = apem.id_player ) FROM associacao_player_escolha_minigame apem INNER JOIN tb_escolha_minigame tem ON apem.id_escolha_minigame = tem.id_escolha_minigame;"
     );
 };
 
